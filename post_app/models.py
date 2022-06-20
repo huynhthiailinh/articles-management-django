b6a4e6899ebe8f0bc6a5c5f73137f9aba7bbcf86
HUYNHTHIAILINH <huynhthiailinh2105@gmail.com>
2022-06-20 11:49:10 +0700

    create Topic model

--- post_app/models.py
+++ post_app/models.py
@@ -3,11 +3,18 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
 
+class Topic(models.Model):
+    name = models.TextField(max_length=256)
+
+    def __str__(self):
+        return self.name
+
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
